0336fd85b53f74c0e7127642f6af28b282b4c842
Elizaveta Veselova <veselovae1605@yandex.ru>
2024-04-14 22:19:38 +0300

    feat: add create card and btn

--- src/components/CardsBox.vue
+++ src/components/CardsBox.vue
@@ -10,6 +10,10 @@
                 <label for="definition">definition</label>
             </div>
         </div>
+
+        <button class="btn-create-card">
+            <img src="../assets/arrow-down-solid.svg" alt="create" />
+        </button>
     </div>
 </template>
 
@@ -18,20 +22,66 @@
 </script>
 
 <style lang="scss" scoped>
-    .create-card {
-        /*:first-child {
-            margin-bottom: 10px;
-        }*/
+    .cards-box {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
 
-        .box {
-            input {
-                display: block;
+        .create-card {
+            background-color: #efbc75;
+            padding: 13px 20px;
+            border-radius: 10px;
+            min-width: 250px;
+
+            .box {
+                display: flex;
+                flex-direction: column;
+
+                input {
+                    width: 100%;
+                    font-size: 16px;
+                    padding: 3px 0;
+                    color: #1a4a5a;
+                    font-weight: 500;
+                    background-color: transparent;
+                    outline: none;
+                    border: 0;
+                    border-bottom: 2px solid #148d8d; /*1a4a5a*/
+                    transition: border-color 0.1s;
+
+                    &:focus {
+                        border-color: #1a4a5a;
+                    }
+                }
+
+                label {
+                    color: #1a4a5a;
+                    margin-top: 3px;
+                    font-size: 13px;
+                    user-select: none;
+                }
+            }
+
+            .term-box {
+                margin-bottom: 7px;
             }
         }
 
-        /*[class*="box"] {
+        .btn-create-card {
+            border-radius: 50%;
+            padding: 10px;
             display: flex;
-            flex-direction: column;
-        }*/
+            align-items: center;
+            justify-content: center;
+            border: none;
+            background-color: #efbc75;
+            cursor: pointer;
+
+            img {
+                height: 20px;
+                width: 20px;
+            }
+        }
     }
 </style>
