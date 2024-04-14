5c2186bac9379fb23e31fad4cea19d4d88901c54
Elizaveta Veselova <veselovae1605@yandex.ru>
2024-04-14 21:22:53 +0300

    init

---
+++ src/components/CardsBox.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="cards-box">
+        <div class="create-card">
+            <div class="term-box box">
+                <input type="text" id="term" />
+                <label for="term">term</label>
+            </div>
+            <div class="definition-box box">
+                <input type="text" id="definition" />
+                <label for="definition">definition</label>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style lang="scss" scoped>
+    .create-card {
+        /*:first-child {
+            margin-bottom: 10px;
+        }*/
+
+        .box {
+            input {
+                display: block;
+            }
+        }
+
+        /*[class*="box"] {
+            display: flex;
+            flex-direction: column;
+        }*/
+    }
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import Vuex from "vuex";
+// import products from "./modules/products";
+
+export default new Vuex.Store({
+    modules: {
+        // products,
+    },
+});
