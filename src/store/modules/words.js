30a4a268f6f8971f0070f321fd49754e641c5bd8
Elizaveta Veselova <veselovae1605@yandex.ru>
2024-04-30 13:13:04 +0300

    add vue3-otp-input

--- src/store/modules/words.js
+++ src/store/modules/words.js
@@ -23,10 +23,10 @@ export default {
     },
     state: {
         words: [
+            ["computer", "компьютер"],
             ["home", "дом"],
             ["tree", "дерево"],
             ["girl", "девочка"],
-            ["computer", "компьютер"],
             ["mouse", "мышь"],
         ],
         index: 2,
